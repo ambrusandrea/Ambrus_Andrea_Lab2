417e1b0269a1dcbb83f11188483c52b29f8a417b
Ambrus Andrea <ambrusandrea25@gmail.com>
2022-10-30 12:53:28 +0200

    Add project files.

---
+++ Data/Ambrus_Andrea_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ambrus_Andrea_Lab2.Models;
+
+namespace Ambrus_Andrea_Lab2.Data
+{
+    public class Ambrus_Andrea_Lab2Context : DbContext
+    {
+        public Ambrus_Andrea_Lab2Context (DbContextOptions<Ambrus_Andrea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ambrus_Andrea_Lab2.Models.Book> Book { get; set; } 
+
+        public DbSet<Ambrus_Andrea_Lab2.Models.Publisher> Publisher { get; set; } 
+
+        public DbSet<Ambrus_Andrea_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Pages/Authors/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ambrus_Andrea_Lab2.Data;
+using Ambrus_Andrea_Lab2.Models;
+
+namespace Ambrus_Andrea_Lab2.Pages.Authors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ambrus_Andrea_Lab2.Data.Ambrus_Andrea_Lab2Context _context;
+
+        public IndexModel(Ambrus_Andrea_Lab2.Data.Ambrus_Andrea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Author> Author { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Author != null)
+            {
+                Author = await _context.Author.ToListAsync();
+            }
+        }
+    }
+}
