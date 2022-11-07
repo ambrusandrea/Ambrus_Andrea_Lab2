8974aca58a1ac9246f742ac8ddebcd138b538c75
Ambrus Andrea <ambrusandrea25@gmail.com>
2022-11-07 16:26:40 +0200

    laborator3

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Ambrus_Andrea_Lab2.Models;
+namespace Ambrus_Andrea_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
