dac40b443778add172cc9578033f8c7b7f939525
Ambrus Andrea <ambrusandrea25@gmail.com>
2022-11-15 19:35:04 +0200

    lab5

--- Migrations/20221109135636_Borrowings.Designer.cs
+++ Migrations/20221109135636_Borrowings.Designer.cs
@@ -4,6 +4,7 @@ using Ambrus_Andrea_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ambrus_Andrea_Lab2.Migrations
 {
     [DbContext(typeof(Ambrus_Andrea_Lab2Context))]
-    partial class Ambrus_Andrea_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221109135636_Borrowings")]
+    partial class Borrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -99,32 +101,6 @@ namespace Ambrus_Andrea_Lab2.Migrations
                     b.ToTable("BookCategory");
                 });
 
-            modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Borrowing", b =>
-                {
-                    b.Property<int>("ID")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ID"), 1L, 1);
-
-                    b.Property<int?>("BookID")
-                        .HasColumnType("int");
-
-                    b.Property<int?>("MemberID")
-                        .HasColumnType("int");
-
-                    b.Property<DateTime>("ReturnDate")
-                        .HasColumnType("datetime2");
-
-                    b.HasKey("ID");
-
-                    b.HasIndex("BookID");
-
-                    b.HasIndex("MemberID");
-
-                    b.ToTable("Borrowing");
-                });
-
             modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Category", b =>
                 {
                     b.Property<int>("ID")
@@ -140,35 +116,6 @@ namespace Ambrus_Andrea_Lab2.Migrations
                     b.HasKey("ID");
 
                     b.ToTable("Category");
-                });
-
-            modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Member", b =>
-                {
-                    b.Property<int>("ID")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ID"), 1L, 1);
-
-                    b.Property<string>("Adress")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("FirstName")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("LastName")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Phone")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("ID");
-
-                    b.ToTable("Member");
                 });
 
             modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Publisher", b =>
@@ -222,21 +169,6 @@ namespace Ambrus_Andrea_Lab2.Migrations
                     b.Navigation("Category");
                 });
 
-            modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Borrowing", b =>
-                {
-                    b.HasOne("Ambrus_Andrea_Lab2.Models.Book", "Book")
-                        .WithMany()
-                        .HasForeignKey("BookID");
-
-                    b.HasOne("Ambrus_Andrea_Lab2.Models.Member", "Member")
-                        .WithMany("Borrowings")
-                        .HasForeignKey("MemberID");
-
-                    b.Navigation("Book");
-
-                    b.Navigation("Member");
-                });
-
             modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Author", b =>
                 {
                     b.Navigation("Books");
@@ -250,11 +182,6 @@ namespace Ambrus_Andrea_Lab2.Migrations
             modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Category", b =>
                 {
                     b.Navigation("BookCategories");
-                });
-
-            modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Member", b =>
-                {
-                    b.Navigation("Borrowings");
                 });
 
             modelBuilder.Entity("Ambrus_Andrea_Lab2.Models.Publisher", b =>
---
+++ Pages/Borrowings/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ambrus_Andrea_Lab2.Data;
+using Ambrus_Andrea_Lab2.Models;
+
+namespace Ambrus_Andrea_Lab2.Pages.Borrowings
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ambrus_Andrea_Lab2.Data.Ambrus_Andrea_Lab2Context _context;
+
+        public IndexModel(Ambrus_Andrea_Lab2.Data.Ambrus_Andrea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Borrowing> Borrowing { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Borrowing != null)
+            {
+                Borrowing = await _context.Borrowing
+                   .Include(b => b.Book)
+                        .ThenInclude(b => b.Author)
+                   .Include(b => b.Member).ToListAsync();
+            }
+        }
+    }
+}
